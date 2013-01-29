a39c03ae2f231c6343046e29111e4854409f7a09
Joseph Crandall <joe@Joe-Inspiron-N5050.(none)>
2013-01-29 16:05:59 -0500

    Updated .gitignore

--- monitoring/models.py
+++ monitoring/models.py
@@ -3,21 +3,20 @@ from django.utils import timezone
 from datetime import datetime
 
 class Sensor(models.Model):
-    id_number = models.IntegerField()
-    nickname = models.CharField(max_length=512)
+	id_number = models.IntegerField()
+	nickname = models.CharField(max_length=512)
 
-    def __unicode__(self):
+	def __unicode__(self):
 		return unicode(self.nickname)
 	
 class Data(models.Model):
-    sensor_id = models.IntegerField(Sensor)
-    time = models.DateTimeField()
-    temp = models.FloatField()
-    data = models.IntegerField()
-
-    def lastestData(self):
-        return self.data >= timezone.now() - datetime.timedelta(minutes=1)
+	sensor = models.ForeignKey(Sensor)
+	time = models.DateTimeField()
+	temp = models.FloatField()
+    
+	def lastestData(self):
+        	return self.data >= timezone.now() - datetime.timedelta(minutes=1)
 	
 	def __unicode__(self):
-		return unicode(self.sensor_id.nickname)
+		return unicode('%s:%s:%f' % (self.sensor.nickname, self.time, self.temp))
 
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render_to_response
-from monitoring.models import Sensor
+from monitoring.models import *
 
 def index(request):
-    sensor_list = Sensor.objects.all().order_by('-id_number')[:4]
-    return render_to_response('monitoring/index.html',{'sensor_list': sensor_list})
+	sensor_list = Sensor.objects.all().order_by('-id_number')[:4]
+	data_list_list = list()
+	
+	for sensor in sensor_list:
+		data_list = Data.objects.filter(sensor__pk = sensor.pk).order_by('time')
+		data_list_list.append(data_list)	
+
+	return render_to_response('monitoring/index.html',{'sensor_list': sensor_list, 'data_list_list':data_list_list})
